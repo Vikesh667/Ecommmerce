46879b84d3a703409a17efecbb6005187084720f
Vikesh667 <114086050+Vikesh667@users.noreply.github.com>
2023-05-21 19:31:58 +0530

    SingUp Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Footer from './component/Footer';
+import Nav from './component/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUp from './component/SignUp';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='App'>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<h1>Products</h1>}/>
+        <Route path='/add' element={<h1>Add Products</h1>}/>
+        <Route path='/update' element={<h1>Update Products</h1>}/>
+        <Route path='/profile' element={<h1>Profile</h1>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
---
+++ src/component/SignUp.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate=useNavigate()
+  const registerUser=async(e)=>{
+     e.preventDefault()
+     let result=await fetch("http://localhost:5000/register",{
+      method:"post",
+      body:JSON.stringify({name,email,password}),
+      headers:{
+        'Content-Type':'application/json'
+      }
+     })
+     result=await result.json()
+        localStorage.setItem("User",JSON.stringify(result))
+       navigate('/')
+    }
+  return (
+    <div className="register">
+      <h1>Register</h1>
+      <input className="inputBox"
+       type="text"
+       value={name} onChange={(e)=>setName(e.target.value)}
+        placeholder="Enter Name" />
+      <input className="inputBox"
+       type="text"
+       value={email} onChange={(e)=>setEmail(e.target.value)}
+        placeholder="Enter Email" />
+      <input
+        className="inputBox"
+        type="password"
+        value={password} onChange={(e)=>setPassword(e.target.value)}
+        placeholder="Enter Password"
+      />
+      <button className="AppButton" type="button" onClick={registerUser}>
+        Sign Up
+      </button>
+    </div>
+  );
+};
+
+export default SignUp;
